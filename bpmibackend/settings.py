38e9fded982a1f00795603ca43be5095bf0f47aa
Nwokolo Matthew <markothedevmail@gmail.com>
2023-11-29 14:18:43 +0100

    added settings for collectstatic

--- bpmibackend/settings.py
+++ bpmibackend/settings.py
@@ -119,7 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+# STATIC_ROOT=Path(BASE_DIR,'static')
+STATIC_ROOT = BASE_DIR / "staticfiles"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
